0b67d2940c69a9106eb335b587f2168af999f8fe
anacristea1991 <92662717+anacristea1991@users.noreply.github.com>
2021-11-06 09:19:18 +0200

    Inventory tab functionality added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,8 +31,10 @@ namespace Cristea_Anamaria_Lab5
     public partial class MainWindow : Window
     {
         ActionState action = ActionState.Nothing;
+        ActionState actionI = ActionState.Nothing;
         AutoLotEntitiesModel ctx = new AutoLotEntitiesModel();
         CollectionViewSource customerViewSource;
+        CollectionViewSource inventoryViewSource;
         public MainWindow()
         {
             InitializeComponent();
@@ -45,6 +47,10 @@ namespace Cristea_Anamaria_Lab5
                         ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
             customerViewSource.Source = ctx.Customer.Local;
             ctx.Customer.Load();
+            inventoryViewSource =
+                       ((System.Windows.Data.CollectionViewSource)(this.FindResource("inventoryViewSource")));
+            inventoryViewSource.Source = ctx.Inventory.Local;
+            ctx.Inventory.Load();
         }
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
@@ -127,10 +133,13 @@ namespace Cristea_Anamaria_Lab5
             try
             {
                 Customer customer = (Customer)customerDataGrid.SelectedItem;
-                custIdTextBox.Text = customer.CustId.ToString();
-                firstNameTextBox.Text = customer.FirstName;
-                lastNameTextBox.Text = customer.LastName;
-                action = ActionState.Edit;
+                if (customer != null)
+                {
+                    custIdTextBox.Text = customer.CustId.ToString();
+                    firstNameTextBox.Text = customer.FirstName;
+                    lastNameTextBox.Text = customer.LastName;
+                    action = ActionState.Edit;
+                }
             }
             catch (DataException ex)
             {
@@ -140,7 +149,130 @@ namespace Cristea_Anamaria_Lab5
 
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
-            action = ActionState.Delete;
+            Customer customer = (Customer)customerDataGrid.SelectedItem;
+            if (customer != null)
+            {
+                action = ActionState.Delete;
+            }
+        }
+
+        private void btnSaveI_Click(object sender, RoutedEventArgs e)
+        {
+            Inventory inventory = null;
+            if (actionI == ActionState.New)
+            {
+                try
+                {
+                    //instantiem Customer entity
+                    inventory = new Inventory()
+                    {
+                        Color = colorTextBox.Text.Trim(),
+                        Make = makeTextBox.Text.Trim()
+                    };
+                    //adaugam entitatea nou creata in context
+                    ctx.Inventory.Add(inventory);
+                    inventoryViewSource.View.Refresh();
+                    //salvam modificarile
+                    ctx.SaveChanges();
+                }
+                //using System.Data;
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if (action == ActionState.Edit)
+            {
+                try
+                {
+                    inventory = (Inventory)inventoryDataGrid.SelectedItem;
+                    inventory.Color = colorTextBox.Text.Trim();
+                    inventory.Make = makeTextBox.Text.Trim();
+                    //salvam modificarile
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                inventoryViewSource.View.Refresh();
+                // pozitionarea pe item-ul curent
+                inventoryViewSource.View.MoveCurrentTo(inventory);
+            }
+            else if (action == ActionState.Delete)
+            {
+                try
+                {
+                    inventory = (Inventory)inventoryDataGrid.SelectedItem;
+                    ctx.Inventory.Remove(inventory);
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                inventoryViewSource.View.Refresh();
+            }
+        }
+
+        private void btnNewI_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                carIdTextBox.Text = "0";
+                colorTextBox.Text = string.Empty;
+                makeTextBox.Text = string.Empty;
+                actionI = ActionState.New;
+            }
+            catch (DataException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnEditI_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Inventory inventory = (Inventory)inventoryDataGrid.SelectedItem;
+                if (inventory != null)
+                {
+                    actionI = ActionState.Edit;
+                }
+            }
+            catch (DataException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDeleteI_Click(object sender, RoutedEventArgs e)
+        {
+            Inventory inventory = (Inventory)inventoryDataGrid.SelectedItem;
+            if (inventory != null)
+            {
+                actionI = ActionState.Edit;
+            }
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            customerViewSource.View.MoveCurrentToNext();
+        }
+
+        private void btnPrev_Click(object sender, RoutedEventArgs e)
+        {
+            customerViewSource.View.MoveCurrentToPrevious();
+        }
+
+        private void btnNextI_Click(object sender, RoutedEventArgs e)
+        {
+            inventoryViewSource.View.MoveCurrentToNext();
+        }
+
+        private void btnPrevI_Click(object sender, RoutedEventArgs e)
+        {
+            inventoryViewSource.View.MoveCurrentToPrevious();
         }
     }
 }
